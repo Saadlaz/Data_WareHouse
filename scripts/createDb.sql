9588ff23af3787a588f009aeeb6d3b3019c41c4f
Saadlaz <154288640+Saadlaz@users.noreply.github.com>
2025-11-22 14:51:43 +0100

    Create DataWarehouse database and schemas

--- scripts/createDb.sql
+++ scripts/createDb.sql
@@ -1,1 +1,21 @@
 
+-- Switch to the master database (required before creating a new database)
+USE master;
+
+-- Create the main Data Warehouse database
+CREATE DATABASE DataWarehouse;
+
+-- Switch context to the newly created DataWarehouse database
+USE DataWarehouse;
+
+-- Create the Bronze schema (raw data layer)
+CREATE SCHEMA bronze;
+GO
+
+-- Create the Silver schema (cleaned and transformed data layer)
+CREATE SCHEMA silver;
+GO
+
+-- Create the Gold schema (analytics and reporting layer)
+CREATE SCHEMA gold;
+GO
